5f82d5df35cb9011e841e4cbe060c1154dfc43da
Lizbeth <liz.p.salinas23123@gmial.com>
2023-09-25 19:28:19 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'codeology',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
